89211ca3071f42b10dd2e9ec84055a313ade9772
Sushant <sushant.alone@hungama.com>
2020-08-04 19:35:48 +0530

    Initial Commit

---
+++ Jet2AssignmentTests/Jet2AssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  Jet2AssignmentTests.swift
+//  Jet2AssignmentTests
+//
+//  Created by Sushant Alone on 04/08/20.
+//  Copyright © 2020 Ishan Alone. All rights reserved.
+//
+
+import XCTest
+@testable import Jet2Assignment
+
+class Jet2AssignmentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
